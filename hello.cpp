fe4aec373cfe6b5aacf34ffb4c85fc92534b3223
Ariel <arieljose06@gmail.com>
2015-02-05 22:31:17 -0500

    Added hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+
+/*
+Author: Ariel German arieljose06@gmail.com
+This is just me testing out the repository and learning how to use this.
+You should know what this program will do.
+
+
+*/
+int main()
+{
+	cout<<"I do not like you world!. No hello for you!";
+	return 0;
+}
