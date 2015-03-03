68f285ca74776b9fb56b37d7ced5ff2e3b603b25
Ariel <arieljose06@gmail.com>
2015-03-03 11:32:12 -0500

    update

--- wordcount.cpp
+++ wordcount.cpp
@@ -9,9 +9,10 @@
  * YOUR OWN WORK**.
  * References:
  * http://www.cplusplus.com/reference/set/set/
+ * http://www.cplusplus.com/reference/cctype/isspace/
  *
- * Finally, please indicate approximately how many hours you spent on this:
- * #hours: 1hr
+ * Finally, please indicate approlineimately how many hours you spent on this:
+ * #hours: 3hr
  */
 
 #include <iostream>
@@ -22,37 +23,84 @@ using std::endl;
 using std::string;
 #include <set>
 using std::set;
-using std::basic_string;
+using std :: iterator;
+
 // write this function to help you out with the computation.
 unsigned long countWords(const string& s, set<string>& wl);
+//Helps print the set out for debugging
+int printSet(set<string>& words);
+
 
 int main()
-{
-	string x;
-	set<string> s;
-	long words = 0;
-	while(cin>>x)
-	{
-	getline(cin,x);
-	words += countWords(x,s);
+{	
+	//Set up what I need to store for later
+	string currentLine;
+	set<string> uniqueLinesSet; 
+	set<string> uniqueWordSet;
+	long lineCount = 0;
+	long wordCount = 0;
+	long charCount = 0;
+	long uniqueLineCount = 0;
+	
 
+	while(getline(cin ,currentLine)){
+		charCount++;// account for new line char
+		lineCount++;// account for new line received
+		wordCount += countWords(currentLine, uniqueWordSet);//track words
+		charCount += currentLine.size();//track characters
+		uniqueLinesSet.insert(currentLine);//track unique lines
 	}
+	
+	//Print out the formated count
+	cout<< "\t" << lineCount << "\t"<< wordCount<< "\t"
+		<< charCount<< "\t"<< uniqueLinesSet.size()
+			<< "\t"<< uniqueWordSet.size()<< endl;
 
-	cout << "Count is : " << words << endl;
 	return 0;
 }
 
-unsigned long countWords(const string s, set<string>& wl){
-	string word="";
-	basic_string<char> line = s;
-	for(int i = 0;i < s.size();i++){
-		if(line[i] != " " || line[i] != "\n"|| line[i]!= "."){
-			word += line[i];
-		}else{
-			word =  "";
+unsigned long countWords(const string& s, set<string>& wl){
+	string temp = "";
+	int count = 0;
+	//Check each character
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if(isspace(s[i]))//if you find space
+		{
+			if(temp!="")//and temp has a word
+			{
+				wl.insert(temp);//add the word
+				count++;
+				temp = "";//reset the word
+			}	
+		}else
+		{
+			temp += s[i];//else continue adding to word
 		}
 	}
-	wl.insert(x);
-	return wl.size();
+	//If we have something left in temp just add it
+	if(temp != "")
+	{
+		wl.insert(temp);
+		temp = "";
+		count++;
+	}
+	return count;// returns words counted
 }
+
+int printSet(set<string>& words)
+{
+	for (set<string>::iterator i = words.begin(); i!=words.end(); i++) 
+	{
+    	cout << *i << endl;
+	}
+	cout << "Size: " << words.size() << endl;
+	return 0;
+}
+
+
+
+
+
+
 
